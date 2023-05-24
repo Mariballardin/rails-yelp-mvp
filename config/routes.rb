2f68b45025bcb96c05bb19cf3eb155ae6bd194f9
Mariana Ballardin <mariana.ballardin@hotmail.com>
2023-05-24 14:56:23 +0200

    first 5 steps

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :restaurants, only: [:create, :index, :destroy]
+
+  delete "restaurants/:id", to: "restaurants#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
